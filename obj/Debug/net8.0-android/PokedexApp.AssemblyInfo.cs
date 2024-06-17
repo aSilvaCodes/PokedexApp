4d48d297ab840a351e4ec92589b560429bd97d7c
aSilvaCodes <103013501+aSilvaCodes@users.noreply.github.com>
2024-06-17 19:07:30 -0400

    Initial Pokedex Design
    Began creating Pokedex UI. Added a screen and some visual elements.

--- obj/Debug/net8.0-android/PokedexApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/PokedexApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokedexApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+269a8a099ae2b50852674f519faeb49faabb444a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c7424b26b9dd81e1e84ff1bda6d57990efbf9c65")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokedexApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokedexApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
