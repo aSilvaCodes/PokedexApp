c7424b26b9dd81e1e84ff1bda6d57990efbf9c65
aSilvaCodes <103013501+aSilvaCodes@users.noreply.github.com>
2024-06-17 18:23:51 -0400

    Pokemon Model
    Created model for pokemon objects

--- Model/Pokemon.cs
+++ Model/Pokemon.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace PokedexApp.Model
 {
-    internal class Pokemon
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class NextEvolution
     {
+        public string num { get; set; }
+        public string name { get; set; }
     }
+
+    public class Pokemon
+    {
+        public int id { get; set; }
+        public string num { get; set; }
+        public string name { get; set; }
+        public string img { get; set; }
+        public List<string> type { get; set; }
+        public string height { get; set; }
+        public string weight { get; set; }
+        public List<string> weaknesses { get; set; }
+        public List<NextEvolution> next_evolution { get; set; }
+        public List<PrevEvolution> prev_evolution { get; set; }
+    }
+
+    public class PrevEvolution
+    {
+        public string num { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Pokemon> pokemon { get; set; }
+    }
+
+
 }
--- obj/Debug/net8.0-android/PokedexApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/PokedexApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokedexApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+269a8a099ae2b50852674f519faeb49faabb444a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokedexApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokedexApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
